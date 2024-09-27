f2b9c867a848a7b9cc04a1376722b53c534b2e11
spurs6 <3989602700@qq.com>
2024-09-27 16:19:34 +0800

    作业已完成

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,21 @@
-#include <printf.h>
+#include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
+    int n=0,count=1;
+    scanf("%d",&n);
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            count=0;
+            break;
+        }
+    }
+    if(count==1){
+        printf("%d是素数",n);
+    }
+    else{
+        printf("%d不是素数",n);
+    }
+
+
     return 0;
 }
--- level1/p04_goldbach/main.c
+++ level1/p04_goldbach/main.c
@@ -1,6 +1,24 @@
-#include <printf.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int prime(int n);
 int main() {
-    printf("hello world!\n");
-    return 0;
+    int x;
+    scanf_s("%d", &x);//任何大于2的偶数都能拆解成两个素数
+    for (int i = 1; i <= x / 2; i += 2) {
+        if (prime(i) && prime(x - i)) {
+            printf("%d %d\n", i, x - i);
+        }
+    }
+    system("pause");
 }
+int prime(int n) {
+    int x = (int) sqrt(n);
+    for (int i = 2; i <= x; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
