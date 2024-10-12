966e2ae3e3868f80cfd16d59955f38a2f109934a
spurs6 <3989602700@qq.com>
2024-10-12 17:15:09 +0800

    作业已完成

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,6 +1,49 @@
-#include <printf.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+char encryptchar(char a, int shift) {
+    if (isalpha(a)) {
+        if (a >= 'a' && a <= 'z') {
+            char x = 'a';
+            a = (a - x + shift) %26+ x;
+            return a;
+        }
+        else {
+            char x= 'A';
+            a = (a - x + shift)%26 + x;
+        }
+    }
+    else {
+        return a;
+    }
 }
+char decryptchar(char a, int shift) {
+    a = encryptchar(a, 26 - (shift % 26));
+    return a;
+}
+void encryptstring(char* string, int shift) {
+    for (int i = 0; string[i]!= '\0'; i++) {
+        string[i] = encryptchar(string[i], shift);
+    }
+}
+void decryptstring(char* string, int shift) {
+    for (int i = 0; string[i] != '\0'; i++) {
+        string[i] = decryptchar(string[i], shift);
+    }
+}
+int main() {
+    int shift=0;
+    scanf("%d", &shift);
+    char* p=(char*) malloc(sizeof(char)*100);
+    scanf("%s", p);
+    encryptstring(p, shift);
+    printf("%s\n",p);
+    decryptstring(p,shift);
+    printf("%s\n",p);
+    free(p);
+}
+
+
+
+
